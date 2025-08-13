ca736e02f4b85ae0eecb0d85da9cf363a4c80507
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-08-13 11:09:28 +0200

    Fix manager test

--- github_connector/tests/managers/test_github_manager.py
+++ github_connector/tests/managers/test_github_manager.py
@@ -3,9 +3,12 @@ from mt_tools.github_connector.managers.github_manager import GitHubManager
 
 
 class MockGitHubRequestManager:
+    def __init__(self, session_data) -> None:
+        pass
+
     def get_milestone(self, owner: str, repository: str, milestone_nr: int):
         if owner == "owner" and repository == "repository" and milestone_nr == 1:
-            return {"description": "Test Milestone"}
+            return [{"description": "Test Milestone"}]
 
 
 class MockGitHubManager(GitHubManager):
@@ -14,8 +17,6 @@ class MockGitHubManager(GitHubManager):
 
 class TestGitHubManager(TestCase):
     def test_get_milestone_description(self):
-        # TODO: Fix test
-        return
         manager = MockGitHubManager()
         description = manager.get_milestone_description("owner", "repository", 1)
         self.assertEqual(description, "<p>Test Milestone</p>")
