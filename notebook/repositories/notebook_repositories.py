24f975e7251f402ec4c52e437ce31c2521358e51
chrishombach <chris.hombach@gmail.com>
2026-03-04 10:51:21 +0100

    Add pre commit hook

--- notebook/repositories/notebook_repositories.py
+++ notebook/repositories/notebook_repositories.py
@@ -1,7 +1,5 @@
-from mt_tools.notebook.repositories.notebook_data_repositories import NotebookDataRepository
-from mt_tools.notebook.models.notebook_hub_models import NotebookHubValueDate
-from mt_tools.notebook.repositories.notebook_fields_repositories import (
-    NotebookFieldsRepository,
+from mt_tools.notebook.repositories.notebook_data_repositories import (
+    NotebookDataRepository,
 )
 from mt_tools.notebook.models.notebook_hub_models import NotebookHubValueDate
 from baseclasses.repositories.montrek_repository import MontrekRepository
@@ -15,10 +13,11 @@ class NotebookRepository(MontrekRepository):
     def set_annotations(self):
         self.add_satellite_fields_annotations(NotebookSatellite, ["notebook_name"])
 
+
 class NotebookNotebookDatasRepository(NotebookDataRepository):
     def receive(self, apply_filter=True):
         notebook_hub = NotebookHubValueDate.objects.get(
             pk=self.session_data.get("pk")
         ).hub
         return super().receive(apply_filter).filter(notebook_id=notebook_hub.id)
-                
+
